04a5b79dd1a1e5b87e2d2ac92e76613affe6140a
wangjizhou <wangjz_ivan@163.com>
2019-07-19 08:19:06 +0800

    linux

--- ch03-hdfs/src/main/java/FileSystemCat.java
+++ ch03-hdfs/src/main/java/FileSystemCat.java
@@ -1,6 +1,7 @@
 // cc FileSystemCat Displays files from a Hadoop filesystem on standard output by using the FileSystem directly
-import java.io.InputStream;
+//import java.io.InputStream;
 import java.net.URI;
+import java.io.*;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
@@ -14,6 +15,11 @@ public class FileSystemCat {
     String uri = args[0];
     System.out.println(uri);
     Configuration conf = new Configuration();
+
+    Writer writer = new OutputStreamWriter(new FileOutputStream("output.txt"),
+            "UTF-8");
+    Configuration.dumpConfiguration(conf, writer);
+
     FileSystem fs = FileSystem.get(URI.create(uri), conf);
     InputStream in = null;
     try {
