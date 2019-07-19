9bea3fa6565d207b2ea6077f07f9da65064f7ee1
wangjizhou <wangjz_ivan@163.com>
2019-07-19 11:26:42 +0800

    linux

--- ch03-hdfs/src/main/java/FileSystemCat.java
+++ ch03-hdfs/src/main/java/FileSystemCat.java
@@ -1,5 +1,6 @@
 // cc FileSystemCat Displays files from a Hadoop filesystem on standard output by using the FileSystem directly
 //import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.net.URI;
 import java.io.*;
 
@@ -12,13 +13,24 @@ import org.apache.hadoop.io.IOUtils;
 public class FileSystemCat {
 
   public static void main(String[] args) throws Exception {
-    String uri = args[0];
+//    String uri = args[0];
+//    String uri = "hdfs://localhost/testFile2.txt";  // error 
+    String uri = "hdfs://node1:9000/testFile2.txt";
     System.out.println(uri);
     Configuration conf = new Configuration();
 
     Writer writer = new OutputStreamWriter(new FileOutputStream("output.txt"),
             "UTF-8");
     Configuration.dumpConfiguration(conf, writer);
+
+    Class cls = conf.getClass();
+    Field f = cls.getDeclaredField("defaultResources");
+    f.setAccessible(true);
+    // 如果相关的类设置了 SecurityManage，就会报错，Java核心类实现了这个方法，
+    // 阻止私有变量被改变。
+    System.out.println(f.get(conf));
+
+//    System.out.println(Configuration.class.getField(null));
 
     FileSystem fs = FileSystem.get(URI.create(uri), conf);
     InputStream in = null;
